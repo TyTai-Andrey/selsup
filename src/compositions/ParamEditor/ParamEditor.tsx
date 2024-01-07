118cd36ad4275a306a822d7e7bc24b4b0cbfdfb4
Andrey Andriynov <a.andriynov@pa-resultant.ru>
2024-01-07 23:49:10 +0300

    Show model

--- src/compositions/ParamEditor/ParamEditor.tsx
+++ src/compositions/ParamEditor/ParamEditor.tsx
@@ -1,7 +1,7 @@
 import React, { FC, memo, useCallback, useState } from 'react';
 import styles from './ParamEditor.module.scss';
 import { Input } from '@components/Input';
-import { find, map } from 'lodash';
+import { find, map, reduce } from 'lodash';
 
 interface Param {
   id: number;
@@ -27,6 +27,7 @@ interface Color {
 }
 
 export const ParamEditor: FC<Props> = memo(({ params, model }) => {
+  const [isOpenModel, setIsOpenModel] = useState(false);
   const [fields, setFields] = useState(
     map(params, (param) => ({
       ...param,
@@ -54,8 +55,13 @@ export const ParamEditor: FC<Props> = memo(({ params, model }) => {
     [],
   );
 
+  const toggleIsOpenModel = () => setIsOpenModel((prev) => !prev);
+
   return (
     <form className={styles.root}>
+      <button onClick={toggleIsOpenModel} type="button">
+        {isOpenModel ? 'Show model' : 'Hide model'}
+      </button>
       {map(fields, (i) => (
         <Input
           key={i.id}
@@ -67,6 +73,7 @@ export const ParamEditor: FC<Props> = memo(({ params, model }) => {
           isRowDirection
         />
       ))}
+      {isOpenModel && <pre>{JSON.stringify(fields, null, 2)}</pre>}
     </form>
   );
 });
